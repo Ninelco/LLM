8a2cf0b0f6512f0e8a2914ebeff51db0acce9934
Ninelco <ninel.gabdullina@yandex.ru>
2024-07-14 22:32:50 +0300

    Update finetune.py

--- code/finetune.py
+++ code/finetune.py
@@ -30,8 +30,42 @@ for dict in file:
         }
     )
 
-df = pd.DataFrame(data)
-print(df.head(100))
+dataframe = pd.DataFrame(data)
+print(dataframe.head(100))
 
-ds = Dataset.from_pandas(df)
-print(ds)
+dataset = Dataset.from_pandas(dataframe)
+print(dataset)
+
+MODEL_NAME = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
+print(tokenizer.pad_token, tokenizer.eos_token)
+tokenizer.pad_token = tokenizer.eos_token
+
+generation_config = GenerationConfig.from_pretrained(MODEL_NAME)
+print(generation_config)
+
+
+def gen_batches_train():
+    for sample in iter(dataset):
+        # Extract instruction and input from the sample
+        system_prompt = "You are a professional examiner with a deep knowledge of the subject. Your task is to help you compose questions for the student exam."
+        input_text = f"# Question: {sample['question']}\n# Right answer: {sample['correct_answer']}\n\nCreate 3 plausible but incorrect answers (distractors) for this question. Generate 3 incorrect answers (distractors) in the following format:\n# Distractors:\n - <wrong answer 1>\n - <wrong answer 2>\n - <wrong answer 3>.\nDon't add numbers or letters to the answers."
+        out_text = f"# Distractors:\n - {sample['distractor_1']}\n - {sample['distractor_2']}\n - {sample['distractor_3']}"
+        formatted_prompt = None
+
+        formatted_prompt = tokenizer.apply_chat_template([{
+            "role": "system",
+            "content": system_prompt
+        }, {
+            "role": "user",
+            "content": input_text
+        }, {
+            "role": "assistant",
+            "content": out_text
+        }], tokenize=False, add_generation_prompt=False) + '<|end_of_text|>'
+
+        yield {'text': formatted_prompt}
+
+
+next(gen_batches_train())
